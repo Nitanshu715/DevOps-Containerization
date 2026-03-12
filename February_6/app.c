4a6998a1510ca7b396d23a37248370982ac08111
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-03-12 19:24:21 +0530

    Add files via upload

---
+++ February_6/app.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char stored_sapid[] = "500121943";
+    char user_sapid[50];
+
+    while (1) {
+
+        printf("Enter your SAP ID: ");
+        scanf("%s", user_sapid);
+
+        if (strcmp(user_sapid, stored_sapid) == 0) {
+            printf("Matched\n");
+        }
+        else {
+            printf("Not Matched\n");
+        }
+
+    }
+
+    return 0;
+}
